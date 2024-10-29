11d4aad99fcb7b3a256bfcec9827a3f070bd8fb4
Yash Mishra <135659080+yaxsh1102@users.noreply.github.com>
2024-10-29 22:48:20 +0530

    Update socket.ts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import {  io } from 'socket.io-client';
 
-const socket = io("http://localhost:4000/" ,{
+const socket = io("https://chatapp-jgby.onrender.com/" ,{
     reconnectionAttempts: 5,  
     timeout: 10000,          
     autoConnect: true         
